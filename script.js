9380f6e7ff2dde248f44eaebee25c67b2a75a6d4
Kanchan Abhay Patil <patilkanchan2002@gmail.com>
2023-10-17 20:41:18 +0530

    code clean up, css changes, json updated

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
-    // Fetch data from JSON file
-    $.getJSON('stats111023.json', function(data) {
-      // Iterate over each item in the JSON array
-      $.each(data, (index, item) => {
-              // Append a new row to the table for each item
-              $('#rankings tbody').append('<tr><td>' + item.Rank + '</td><td>' + item.StudentName + '</td><td>' + item.CoursesCompleted + '</td><td>' + item.SkillBadgesCompleted + '</td><td>' + item.GenAIGameCompleted + '</td><td>' + item.TotalCompletions + '</td><td>' + item.RedemptionStatus + '</td></tr>');
-          });
+  // Fetch data from JSON file
+  $.getJSON('data.json', function(data) {
+    var tableBody = $('#rankings tbody');
+
+    // Iterate over each item in the JSON array
+    $.each(data, function(index, item) {
+      // Append a new row to the table for each item
+      var row = $('<tr>').appendTo(tableBody);
+      row.append('<td>' + item.Rank + '</td>');
+      row.append('<td>' + item.StudentName + '</td>');
+      row.append('<td>' + item.CoursesCompleted + '</td>');
+      row.append('<td>' + item.SkillBadgesCompleted + '</td>');
+      row.append('<td>' + item.GenAIGameCompleted + '</td>');
+      row.append('<td>' + item.TotalCompletions + '</td>');
+      row.append('<td>' + item.RedemptionStatus + '</td>');
+
+      if (item.TotalCompletions === "Yes"){
+        row.addClass('completed');
+      }
+      // Apply alternate row colors
+      if (index % 2 === 0) {
+        row.addClass('even-row');
+      } else {
+        row.addClass('odd-row');
+      }
     });
   });
+});
+
 
   $(document).ready(function() {
     $('#search-leaderboard').keyup(function() {
